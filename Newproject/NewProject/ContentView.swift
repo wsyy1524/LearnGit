cf4323b97681b90086322acf71c101bdad88ffd6
yangying <yingy@keyayun.com>
2020-12-14 16:32:10 +0800

    First Commit

---
+++ Newproject/NewProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewProject
+//
+//  Created by 杨莹 on 2020/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Newproject/NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by 杨莹 on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
